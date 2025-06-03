feee51ec1eecf3e126464764acf8054247c84451
Pawel <paul.trzupek@gmail.com>
2025-06-03 12:27:02 +0200

    registering implemented

--- GymManager.Shared/DTOs/Member/RegisterMemberDto.cs
+++ GymManager.Shared/DTOs/Member/RegisterMemberDto.cs
@@ -1,11 +1,43 @@
 ﻿namespace GymManager.Shared.DTOs.Member;
 
+using System.ComponentModel.DataAnnotations;
+
+public class NotInFutureAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is DateTime dateValue)
+        {
+            return dateValue.Date < DateTime.Today;
+        }
+
+        return true;
+    }
+}
 public class RegisterMemberDto
 {
-    public String FirstName { get; set; }
-    public String LastName { get; set; }
-    public String Email { get; set; }
+    [Required(ErrorMessage = "First name is required.")]
+    [StringLength(50, ErrorMessage = "First name must be max 50 characters.")]
+    public string FirstName { get; set; } = "";
+
+    [Required(ErrorMessage = "Last name is required.")]
+    [StringLength(50, ErrorMessage = "Last name must be max 50 characters.")]
+    public string LastName { get; set; } = "";
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
+    public string Email { get; set; } = "";
+
+    [Required(ErrorMessage = "Date of birth is required.")]
+    [DataType(DataType.Date)]
+    [NotInFuture(ErrorMessage = "Date of birth must be in the past.")]
     public DateTime DateOfBirth { get; set; }
-    public String PhoneNumber { get; set; }
-    public String Password { get; set; }
+
+    [Required(ErrorMessage = "Phone number is required.")]
+    [RegularExpression(@"^\d{9}$", ErrorMessage = "Phone number must be exactly 9 digits.")]
+    public string PhoneNumber { get; set; } = "";
+
+    [Required(ErrorMessage = "Password is required.")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters.")]
+    public string Password { get; set; } = "";
 }
