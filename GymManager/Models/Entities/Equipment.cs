a6dd2b814038e75d801031c1d2a9339002c0ca8c
Patryk Persak <abogusiac@gmail.com>
2025-06-03 15:03:34 +0200

    Add equipment photo field and UI to /equipment

--- GymManager/Models/Entities/Equipment.cs
+++ GymManager/Models/Entities/Equipment.cs
@@ -21,6 +21,9 @@ public class Equipment
     
     [StringLength(300)]
     public string? Notes { get; set; }  // -> dto get: chcemy
-    
+
+    [Required]
+    public string PhotoPath { get; set; }
+
     public ICollection<ServiceRequest> ServiceRequests { get; set; } = new List<ServiceRequest>(); // -> chcemy wysweitlac adminiowi zeby mogl przydzielic?
 }
