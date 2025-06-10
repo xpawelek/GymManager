b3c6f100e142f094d784e5e37d8426efa7816e9f
Patryk Persak <abogusiac@gmail.com>
2025-06-10 02:18:49 +0200

    Add endpoints tests

---
+++ GymManager.Tests/Helpers/TestHelper.cs
@@ -0,0 +1,9 @@
+﻿using Moq;
+using Microsoft.Extensions.Logging;
+
+namespace GymManager.Tests.Helpers;
+
+public static class TestHelper
+{
+    public static ILogger<T> GetLogger<T>() => new Mock<ILogger<T>>().Object;
+}
