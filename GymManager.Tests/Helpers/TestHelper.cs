26d93c3d0a75f2d37062e519829880b122413561
Patryk Persak <126697013+persi05@users.noreply.github.com>
2025-06-10 03:24:26 +0200

    Delete GymManager.Tests/Helpers directory

--- GymManager.Tests/Helpers/TestHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using Moq;
-using Microsoft.Extensions.Logging;
-
-namespace GymManager.Tests.Helpers;
-
-public static class TestHelper
-{
-    public static ILogger<T> GetLogger<T>() => new Mock<ILogger<T>>().Object;
-}
